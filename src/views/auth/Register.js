790131ebe808f398c034db72c735a55905a0255f
Rifky <rifkyrahma0420@gmail.com>
2021-02-12 15:57:09 +0700

    menambahkan validation error

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -5,6 +5,8 @@ const [name, setName] = useState('')
 const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
 const [password_confirmation, setPasswordConfirmation] = useState('')
+const [errors, setErrors] = useState([])
+const [message, setMessage] = useState('')
     
 const record =  { name, email, password, password_confirmation }
 const store = async (e) => {
@@ -16,7 +18,8 @@ const store = async (e) => {
     setPassword = ('')
     setPasswordConfirmation = ('')
    } catch (e) {
-       console.log(e.response);
+       setErrors(e.response.data.errors);
+       setMessage(e.response.data.message);
    }
 }
 
@@ -24,21 +27,31 @@ const store = async (e) => {
         <div className="container">
             <div className="row">
                 <div className="col-md-5">
+                    { message ? <div className="alert alert-danger">{message}</div> : '' }
                     <div className="card">
                         <div className="card-header">Register</div>
                         <div className="card-body">
                             <form onSubmit={store}>
                                 <div className="mb-4">
                                     <label htmlFor="name" className="form-label">Name</label>
-                                    <input type="text" onChange={(e)=> setName(e.target.value)} value={name} name="name" id="name" className="form-control"/>
+                                    <input type="text" onChange={(e) => setName(e.target.value)} value={name} name="name" id="name" className={ `form-control ${errors.name ? 'is-invalid': ''}`}/>
+                                    {
+                                        errors.name ? <div className="invalid-feedback">{errors.name[0]}</div> : ''
+                                    }
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="email" className="form-label">email</label>
-                                    <input type="email" onChange={(e)=> setEmail(e.target.value)} value={email} name="email" id="email" className="form-control"/>
+                                    <input type="email" onChange={(e)=> setEmail(e.target.value)} value={email} name="email" id="email" className={ `form-control ${errors.email ? 'is-invalid': ''}`}/>
+                                    {
+                                        errors.email ? <div className="invalid-feedback">{errors.email[0]}</div> : ''
+                                    }
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="password" className="form-label">Password</label>
-                                    <input type="password" onChange={(e)=> setPassword(e.target.value)} value={password} name="password" id="password" className="form-control"/>
+                                    <input type="password" onChange={(e)=> setPassword(e.target.value)} value={password} name="password" id="password" className={ `form-control ${errors.email ? 'is-invalid': ''}`}/>
+                                    {
+                                        errors.password ? <div className="invalid-feedback">{errors.password[0]}</div> : ''
+                                    }
                                 </div>
                                 <div className="mb-4">
                                     <label htmlFor="password_confirmation" className="form-label">Confirm Password</label>
